1e94c2dc6f109ae8df9b3025d818fd9927891fd5
Jesse Noller <jesse.noller@rackspace.com>
2013-05-22 16:55:45 -0400

    remove backup dir

--- templates.bak/home-repos-other.php
+++
@@ -1,4 +0,0 @@
-                        <div class="tab-pane " id="home-repos-other">
-                        	<h3 class="narrow">Other Projects</h3>
-
-                        </div>
--- templates.bak/home-social.php
+++
@@ -1,9 +0,0 @@
-
-
-        <div class="container">
-            <header class="section-header">
-                <h2 class="section-title lite">On the web</h2>
-            </header>
-            <div id="social-feed" class="widget clearfix"></div>
-            <br>
-        </div>
