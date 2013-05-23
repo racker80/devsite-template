6d2b13af3db747305f4dff70c9e10a2783803506
Jesse Noller <jesse.noller@rackspace.com>
2013-05-23 13:11:39 -0400

    try button

--- templates/hero-python.php
+++ templates/hero-python.php
@@ -62,8 +62,8 @@
                         </dd>
                     </dl>
                 </div>
-                <div class="span12">
-                    <a class="btn btn-large btn-success pull-right" href="https://github.com/rackspace/pyrax">Get the Python SDK now! <i class="icon-chevron-right"></i></a>
+                <div class="span8">
+                    <a class="btn btn-large btn-success" href="https://github.com/rackspace/pyrax">Get the Python SDK now! <i class="icon-chevron-right"></i></a>
                 </div>
             </div><!-- row -->
 
