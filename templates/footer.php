d30fb4520f1b9b3b46e18e312ca953acd9baaa8a
eric mcgregor <eric.mcgregor@rackspace.com>
2013-05-23 10:17:31 -0500

    Footer update!

--- templates/footer.php
+++ templates/footer.php
@@ -1,150 +1,154 @@
 <div id="footer">
   
   <div id="rackFooter" class="container">
-    <div class="row-fluid">
-      <div class="span2 push_0">
-        <div>
-          <a href="http://rackspace.com/" target="_blank">Rackspace</a>
+<div class="row-fluid">
+      <div class="span9">
+        <div class="row-fluid">
+          <div class="col span3">
+            <div class="colheader">
+              <a href="http://rackspace.com/" target="_blank">Rackspace</a>
+            </div>
+            <ul>
+              <li>
+                <a href="http://rackspace.com/information/aboutus/" class="footer" target="_blank">About Rackspace Hosting<sup>®</sup></a>
+              </li>
+              <li>
+                <a href="http://rackspace.com/whyrackspace/support/" class="footer" target="_blank">Fanatical Support<sup>®</sup></a>
+              </li>
+              <li>
+                <a href="http://rackspace.com/hosting_solutions/" class="footer" target="_blank">Hosting Solutions</a>
+              </li>
+              <li>
+                <a href="http://rackspace.com/information/hosting101/" class="footer" target="_blank">Web Hosting 101</a>
+              </li>
+              <li>
+                <a href="http://rackspace.com/partners/" class="footer" target="_blank">Hosting Partner Programs</a>
+              </li>
+              <li>
+                <a href="http://rackspace.com/cloudbuilders/openstack/" class="footer" target="_blank">OpenStack<sup>®</sup></a>
+              </li>
+            </ul>
+          </div>
+          <div class="col span3 ">
+            <div class="colheader">
+              <a href="http://rackspace.com/managed_hosting/" target="_blank">Managed Hosting</a>
+            </div>
+            <ul>
+              <li>
+                <a href="http://rackspace.com/managed_hosting/configurations/" class="footer" target="_blank">Managed Configurations</a>
+              </li>
+              <li>
+                <a href="http://rackspace.com/managed_hosting/private_cloud/" class="footer" target="_blank">Managed Private Clouds</a>
+              </li>
+              <li>
+                <a href="http://rackspace.com/managed_hosting/managed_colocation/" class="footer" target="_blank">Managed Colocation Servers</a>
+              </li>
+              <li>
+                <a href="http://rackspace.com/managed_hosting/dedicated_servers/" class="footer" target="_blank">Dedicated Servers</a>
+              </li>
+              <li>
+                <a href="http://rackspace.com/managed_hosting/support/customers/" class="footer" target="_blank">Managed Customers</a>
+              </li>
+              <li>
+                <a href="https://my.rackspace.com" class="footer" rel="nofollow">MyRackspace<sup>®</sup> Portal</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col span3 ">
+            <div class="colheader">
+              <a href="http://rackspace.com/cloud/" target="_blank">Cloud Hosting</a>
+            </div>
+            <ul>
+              <li>
+                <a href="http://rackspace.com/cloud/servers/" class="footer" target="_blank">Cloud Servers™</a>
+              </li>
+              <li>
+                <a href="http://rackspace.com/cloud/sites/" class="footer" target="_blank">Cloud Sites™</a>
+              </li>
+              <li>
+                <a href="http://rackspace.com/cloud/load-balancing/" class="footer" target="_blank">Cloud Load Balancers</a>
+              </li>
+              <li>
+                <a href="http://rackspace.com/cloud/files/" class="footer" target="_blank">Cloud Files™</a>
+              </li>
+              <li>
+                <a href="http://rackspace.com/cloud/cloud_hosting_demos/" class="footer" target="_blank">Cloud Hosting Demos</a>
+              </li>
+              <li>
+                <a href="https://manage.rackspacecloud.com/pages/Login.jsp" class="footer" target="_blank">Cloud Control Panel</a>
+              </li>
+              <li>
+                <a href="https://mycloud.rackspace.com" class="footer" target="_blank">The Rackspace Open Cloud</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col span3 ">
+            <div class="colheader">
+              <a href="http://rackspace.com/apps/" target="_blank">Email &amp; Apps</a>
+            </div>
+            <ul>
+              <li>
+                <a href="http://rackspace.com/apps/email_hosting/" class="footer" target="_blank">Rackspace Email Hosting</a>
+              </li>
+              <li>
+                <a href="http://rackspace.com/apps/email_hosting/exchange_hosting/" class="footer" target="_blank">Microsoft Hosted Exchange</a>
+              </li>
+              <li>
+                <a href="http://rackspace.com/apps/email_hosting/compare/" class="footer" target="_blank">Compare Hosted Products</a>
+              </li>
+              <li>
+                <a href="http://rackspace.com/apps/email_hosting/email_archiving/" class="footer" target="_blank">Email Archiving</a>
+              </li>
+              <li>
+                <a href="http://apps.rackspace.com/" class="footer" target="_blank">Customer Log-in</a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <ul>
-          <li>
-            <a href="http://rackspace.com/information/aboutus/" class="footer" target="_blank">About Rackspace Hosting<sup>®</sup></a>
-          </li>
-          <li>
-            <a href="http://rackspace.com/whyrackspace/support/" class="footer" target="_blank">Fanatical Support<sup>®</sup></a>
-          </li>
-          <li>
-            <a href="http://rackspace.com/hosting_solutions/" class="footer" target="_blank">Hosting Solutions</a>
-          </li>
-          <li>
-            <a href="http://rackspace.com/information/hosting101/" class="footer" target="_blank">Web Hosting 101</a>
-          </li>
-          <li>
-            <a href="http://rackspace.com/partners/" class="footer" target="_blank">Hosting Partner Programs</a>
-          </li>
-          <li>
-            <a href="http://rackspace.com/cloudbuilders/openstack/" class="footer" target="_blank">OpenStack<sup>®</sup></a>
-          </li>
-        </ul>
+
       </div>
-      <div class="span2 push_1">
-        <div>
-          <a href="http://rackspace.com/managed_hosting/" target="_blank">Managed Hosting</a>
-        </div>
-        <ul>
-          <li>
-            <a href="http://rackspace.com/managed_hosting/configurations/" class="footer" target="_blank">Managed Configurations</a>
-          </li>
-          <li>
-            <a href="http://rackspace.com/managed_hosting/private_cloud/" class="footer" target="_blank">Managed Private Clouds</a>
-          </li>
-          <li>
-            <a href="http://rackspace.com/managed_hosting/managed_colocation/" class="footer" target="_blank">Managed Colocation Servers</a>
-          </li>
-          <li>
-            <a href="http://rackspace.com/managed_hosting/dedicated_servers/" class="footer" target="_blank">Dedicated Servers</a>
-          </li>
-          <li>
-            <a href="http://rackspace.com/managed_hosting/support/customers/" class="footer" target="_blank">Managed Customers</a>
-          </li>
-          <li>
-            <a href="https://my.rackspace.com" class="footer" rel="nofollow">MyRackspace<sup>®</sup> Portal</a>
-          </li>
-        </ul>
-      </div>
-      <div class="span2 push_2">
-        <div>
-          <a href="http://rackspace.com/cloud/" target="_blank">Cloud Hosting</a>
-        </div>
-        <ul>
-          <li>
-            <a href="http://rackspace.com/cloud/servers/" class="footer" target="_blank">Cloud Servers™</a>
-          </li>
-          <li>
-            <a href="http://rackspace.com/cloud/sites/" class="footer" target="_blank">Cloud Sites™</a>
-          </li>
-          <li>
-            <a href="http://rackspace.com/cloud/load-balancing/" class="footer" target="_blank">Cloud Load Balancers</a>
-          </li>
-          <li>
-            <a href="http://rackspace.com/cloud/files/" class="footer" target="_blank">Cloud Files™</a>
-          </li>
-          <li>
-            <a href="http://rackspace.com/cloud/cloud_hosting_demos/" class="footer" target="_blank">Cloud Hosting Demos</a>
-          </li>
-          <li>
-            <a href="https://manage.rackspacecloud.com/pages/Login.jsp" class="footer" target="_blank">Cloud Control Panel</a>
-          </li>
-          <li>
-            <a href="https://mycloud.rackspace.com" class="footer" target="_blank">The Rackspace Open Cloud</a>
-          </li>
-        </ul>
-      </div>
-      <div class="span2 push_3">
-        <div>
-          <a href="http://rackspace.com/apps/" target="_blank">Email &amp; Apps</a>
-        </div>
-        <ul>
-          <li>
-            <a href="http://rackspace.com/apps/email_hosting/" class="footer" target="_blank">Rackspace Email Hosting</a>
-          </li>
-          <li>
-            <a href="http://rackspace.com/apps/email_hosting/exchange_hosting/" class="footer" target="_blank">Microsoft Hosted Exchange</a>
-          </li>
-          <li>
-            <a href="http://rackspace.com/apps/email_hosting/compare/" class="footer" target="_blank">Compare Hosted Products</a>
-          </li>
-          <li>
-            <a href="http://rackspace.com/apps/email_hosting/email_archiving/" class="footer" target="_blank">Email Archiving</a>
-          </li>
-          <li>
-            <a href="http://apps.rackspace.com/" class="footer" target="_blank">Customer Log-in</a>
-          </li>
-        </ul>
-      </div>
-      <div class="span2 push_4 contact-info">
-        <div>
+      <div class="col span3 contact_col">
+        <div class="colheader">
           <a href="http://rackspace.com/information/contactus/" rel="nofollow">Contact Us</a>
         </div>
-        <div>
-          <a href=""></a>
-        </div>
-        <div class="column_1">
-          <div class="footerIcon salesIcon"></div><span style="color:#4F81A6;">Sales</span>
-        </div>
-        <div class="column_2">
-          1-800-961-2888
-        </div>
-        <div class="clear"></div>
-        <div class="column_1">
-          <a href="http://rackspace.com/support/" target="_blank"></a>
-          <div class="footerIcon supportIcon"></div><span style="position:relative; color:#4F81A6;">Support</span>
-        </div>
-        <div class="column_2">
-          1-800-961-4454
-        </div>
-        <div class="clear"></div><br>
-        <div>
+        <ul>
+          <li class="footerIcon sales"><span style="color:#4F81A6;">Sales</span> 1-800-961-2888</li>
+          <li class="footerIcon support"><span style="color:#4F81A6;">Support</span> 1-800-961-4454</li>
+        </ul>
+
+        <div class="colheader">
           <a href="http://rackspace.com/information/contactus/" rel="nofollow" target="_blank">Connect With Us</a>
         </div>
-        <div>
-          <a href=""></a>
+        <div class="social_container">
+          <a class="social linkedin" href="http://www.linkedin.com/company/rackspace-hosting/" target="_blank"></a>
+          <a class="social facebook" href="http://www.facebook.com/rackspacehost" target="_blank"></a>
+          <a class="social twitter" href="http://twitter.com/rackspace" target="_blank"></a>
+          <a class="social googleplus" href="https://plus.google.com/100698655987920162334" target="_blank"></a>
+          <a class="social linktous" href="http://rackspace.com/information/links/" target="_blank"></a>
+          <a class="social email" href="http://rackspace.com/forms/contactsales/" target="_blank"></a>
         </div>
-        <a class="social linkedin" href="http://www.linkedin.com/company/rackspace-hosting/" target="_blank"></a>
-        <a class="social facebook" href="http://www.facebook.com/rackspacehost" target="_blank"></a>
-        <a class="social twitter" href="http://twitter.com/rackspace" target="_blank"></a>
-        <a class="social googleplus" href="https://plus.google.com/100698655987920162334" target="_blank"></a>
-        <a class="social linktous" href="http://rackspace.com/information/links/" target="_blank"></a>
-        <a class="social email" href="http://rackspace.com/forms/contactsales/" target="_blank"></a>
-        <div class="clear"></div>
       </div>
-      <div class="clear"></div>
     </div>
 
+    <div id="rackerpowered"></div>
     
   </div>  
 
 
+</div>
+<div id="basement-wrap" class="basement-wrap-nosnap">
+  <div class="container">
+        ©2013 Rackspace, US Inc.
+        <span class="footerlink"><a href="http://rackspace.com/information/aboutus/" class="basement">About Rackspace</a></span> | 
+        <span class="footerlink"><a href="http://rackspace.com/whyrackspace/support/" class="basement">Fanatical Support<sup>®</sup></a></span> | 
+        <span class="footerlink"><a href="http://rackspace.com/hosting_solutions/" class="basement">Hosting Solutions</a></span> | 
+        <span class="footerlink"><a href="http://ir.rackspace.com" class="basement">Investors</a></span> | 
+        <span class="footerlink"><a href="http://www.rackertalent.com" class="basement">Careers</a></span> | 
+        <span class="footerlink"><a href="http://rackspace.com/information/legal/privacystatement/" class="basement">Privacy Statement</a></span> | 
+        <span class="footerlink"><a href="http://rackspace.com/information/legal/websiteterms/" class="basement">Website Terms</a></span> | 
+        <span class="footerlink"><a href="http://rackspace.com/information/legal/copyrights_trademarks/" class="basement">Trademarks</a></span> | 
+        <span class="footerlink"><a href="http://rackspace.com/sitemap/" class="basement" rel="nofollow">Sitemap</a></span>
+  </div>    
 </div>
 
 
