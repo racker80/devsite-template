be341629e81c6fe144df9e19586bf365219d532e
Jesse Noller <jesse.noller@rackspace.com>
2013-05-24 16:50:41 -0400

    test idea

---
+++ templates/home-opensource.php
@@ -0,0 +1,26 @@
+<div class="container section-container">
+  <header class="section-header">
+      <h1 class="section-title lite">Community Projects and Open Source</h1>
+  </header>
+
+  <div class="span4">
+    <img class="img-circle" data-src="static/img/logos/Octocat.png">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+  <div class="span4">
+    <img class="img-circle" data-src="static/img/logos/Octocat.png">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+  </div>
+  <div class="span4">
+    <img class="img-circle" data-src="static/img/logos/Octocat.png">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+  </div>
+
+
+</div> <!-- home-repos -->
+
